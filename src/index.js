f1971bb7b61069ffe3cd4b3279f6c8b52ccd9e64
Jefferson <jefferson.miranda@arctouch.com>
2020-04-10 17:48:38 -0300

    initial game logic

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
-import Phaser from "phaser";
-import logoImg from "./assets/logo.png";
+import Phaser from "phaser"
+import background from "./assets/images/background.png"
+import basketImg from "./assets/images/basket.png"
+import logoImg from "./assets/images/mask.png"
+import alcoolImg from "./assets/images/alcool.png"
+import glovesImg from "./assets/images/gloves.png"
+import soapImg from "./assets/images/soap.png"
+import virusImg from "./assets/images/virus.png"
 
 const config = {
   type: Phaser.AUTO,
   parent: "phaser-example",
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        gravity: { y: 300 },
+        debug: false
+    }
+  },
   scene: {
     preload: preload,
     create: create
@@ -15,18 +28,109 @@ const config = {
 const game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("logo", logoImg);
+  this.load.image('background', background)
+  this.load.image('mask', logoImg)
+  this.load.image('alcool', alcoolImg)
+  this.load.image('gloves', glovesImg)
+  this.load.image('soap', soapImg)
+  this.load.image('virus', virusImg)
+  this.load.image('basket', basketImg)
 }
 
 function create() {
-  const logo = this.add.image(400, 150, "logo");
+  const background = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'background')
+  background.setScale(3)
 
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
+  const basket = this.add.image(200, 200, 'basket')
+  basket.setScale(0.5)
+
+  this.scoreText = this.add.text(basket.x + 140, 165, '0', { fontSize: '80px', fill: '#fff' });
+
+  // this.scoreText.setText(10)
+  
+  const newItem = () => createItem(this)
+// debugger
+  // this.time.delayedCall(1, newItem, null, this)
+
+  this.itemsCreation = this.time.addEvent({
+    delay: 2000,
+    callback: () => {
+      // let newLevel = ++levelClass.currentLevel
+      // levelClass.checkLevel()
+      // this.itemsCreation = setInterval(newItem, 2500)
+      // console.log('level up')
+      // debugger
+      // this.scene.itemsCreation = newItem()
+      newItem()
+    },
+    repeat: -1
+  })
+
+  const currentScene = this
+
+  this.time.addEvent({
+    delay: 5000,
+    callback: () => {
+      // let newLevel = ++levelClass.currentLevel
+      // levelClass.checkLevel()
+      // this.itemsCreation = setInterval(newItem, 2500)
+      console.log('level up')
+      debugger
+      // this.scene.itemsCreation = newItem()
+      // currentScene.itemsCreation.remove()  //newItem()
+      const currentDelay = currentScene.itemsCreation.delay
+      const newDelay = currentDelay - (currentDelay * 0.25)
+
+      if (newDelay > 400) {
+        currentScene.itemsCreation.delay = currentDelay - (currentDelay * 0.25) 
+      }
+
+      console.log(currentScene.itemsCreation.delay)
+
+
+    },
+    repeat: -1
+  })
+
+
+}
+
+function update() {
+  
+}
+
+function createItem(scene) {
+  // console.log('new item')
+  const items = ['mask', 'alcool', 'gloves', 'soap', 'virus']
+
+  const randomItem = items[Math.floor(Math.random() * items .length)];
+
+// console.log(randomItem)
+  var itemSprite = scene.physics.add.image(window.innerWidth / 2, window.innerHeight, randomItem)
+                                   .setScale(0.5).setInteractive()
+  
+  const randomVelocity = Math.random() * (250 - 220) + 220;
+
+  itemSprite.setVelocity(randomVelocity * getNumberSignal());
+  
+  itemSprite.on('pointerdown', function(){
+    const score = this.scene.scoreText
+    const currentScore = score.text
+
+    score.setText(parseInt(currentScore) + 10)
+  });
+
+  scene.tweens.add({
+    targets: itemSprite,
+    y: window.innerHeight / 2,
+    duration: 1000,
     ease: "Power2",
     yoyo: true,
-    loop: -1
+    rotation: getNumberSignal()
+    // loop: -1
   });
+} 
+
+function getNumberSignal() {
+  return (Math.random() - 0.5) * 2
 }
