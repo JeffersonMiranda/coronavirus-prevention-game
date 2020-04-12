182521637547ffae164ac8ee5be0736098a5df13
Jefferson <jefferson.miranda@arctouch.com>
2020-04-12 15:34:57 -0300

    creating menu screen and button to start gameplay

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Phaser from "phaser"
-import background from "./assets/images/background.png"
+import gameplayBackground from "./assets/images/gameplayBackground.png"
+import menuBackground from "./assets/images/menuBackground.png"
 import basketImg from "./assets/images/basket.png"
 import logoImg from "./assets/images/mask.png"
 import alcoolImg from "./assets/images/alcool.png"
@@ -10,6 +11,8 @@ import heartImg from "./assets/images/heart.png"
 import backgroundSong from "./assets/audio/background.wav"
 import hitSound from "./assets/audio/hit.wav"
 import gameOverSound from "./assets/audio/gameover.mp3"
+
+import GamePlayScene from './GamePlayScene'
 
 const config = {
   type: Phaser.AUTO,
@@ -23,16 +26,51 @@ const config = {
         debug: false
     }
   },
+  dom: {
+    createContainer: true
+  },
   scene: {
     preload: preload,
-    create: create
+    create: MenuCreate
   }
 };
 
-const game = new Phaser.Game(config);
+const game = new Phaser.Game(config)
+
+function MenuCreate() {
+  this.background = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'menuBackground')
+  this.background.setScale(3.4)
+
+  const gamePlayScene = new GamePlayScene()
+
+  this.scene.add('gamePlay', GamePlayScene);
+
+  const startStyle = {
+    'background-color': 'green',
+    'max-width': '400px',
+    width: '100%',
+    height: '150px',
+    font: 'bold 48px Arial',
+    color: 'white',
+    'line-height': 3,
+    'text-align': 'center',
+    'border-radius': '25px'
+
+  }
+
+  const startButton = this.add.dom(window.innerWidth / 2, window.innerHeight - 400, 'div', startStyle ,'INICIAR')
+
+  const currentScene = this
+
+  startButton.addListener('click')
+             .on('click', () => {
+              currentScene.scene.start('gamePlay');
+             })
+}
 
 function preload() {
-  this.load.image('background', background)
+  this.load.image('gameplayBackground', gameplayBackground)
+  this.load.image('menuBackground', menuBackground)
   this.load.image('mask', logoImg)
   this.load.image('alcool', alcoolImg)
   this.load.image('gloves', glovesImg)
@@ -43,125 +81,4 @@ function preload() {
   this.load.audio('backgroundSong', backgroundSong);
   this.load.audio('hitSound', hitSound);
   this.load.audio('gameOverSound', gameOverSound);
-}
-
-function create() {
-  this.hitSound = this.sound.add('hitSound');
-  this.gameOverSound = this.sound.add('gameOverSound')
-  this.backgroundSong = this.sound.add('backgroundSong');
-  this.backgroundSong.play({
-    loop: -1
-  })
-
-  this.background = this.add.image(window.innerWidth / 2, window.innerHeight / 2, 'background')
-  this.background.setScale(3)
-
-  this.basket = this.add.image(200, 200, 'basket')
-  this.basket.setScale(0.5)
-
-  this.scoreText = this.add.text(this.basket.x + 140, 165, '0', { fontSize: '80px', fill: '#fff' });
-
-  this.lives = 3
-  this.livesBoard = this.physics.add.staticGroup()
-
-  for (let index = 1; index <= 3; index++) {
-    this.livesBoard.create(window.innerWidth - (70 * index), 200 , 'heart')
-  }
-  
-  const newItem = () => createItem(this)
-
-  this.itemsCreation = this.time.addEvent({
-    delay: 2000,
-    callback: () => {
-      newItem()
-    },
-    repeat: -1
-  })
-
-  const currentScene = this
-
-  this.time.addEvent({
-    delay: 15000,
-    callback: () => {
-      const currentDelay = currentScene.itemsCreation.delay
-      const newDelay = currentDelay - (currentDelay * 0.25)
-
-      if (newDelay > 250) {
-        currentScene.itemsCreation.delay = currentDelay - (currentDelay * 0.25) 
-      }
-    },
-    repeat: -1
-  })
-}
-
-function createItem(scene) {
-  const items = ['mask', 'alcool', 'gloves', 'soap', 'virus']
-  const randomItem = items[Math.floor(Math.random() * items .length)];
-  const randomItemHeightFall = Math.random() * (((window.innerHeight / 2 - 400))  - window.innerHeight / 2 ) + window.innerHeight / 2; 
-
-  var itemSprite = scene.physics.add.image(window.innerWidth / 2, window.innerHeight, randomItem)
-                                     .setScale(0.5)
-                                     .setInteractive()
-
-  itemSprite.isVirus = randomItem === 'virus'                                    
-  
-  const randomVelocity = Math.random() * (250 - 220) + 220;
-
-  itemSprite.setVelocity(randomVelocity * getRandomNumberSignal());
-
-  const itemTween = scene.tweens.add({
-    targets: itemSprite,
-    y: randomItemHeightFall,
-    duration: 800,
-    ease: 'Power2',
-    yoyo: true,
-    rotation: getRandomNumberSignal(),
-    onComplete: () => {
-      if (!itemSprite.isVirus && scene.lives) {
-        scene.livesBoard.getChildren()[scene.lives - 1].alpha = 0
-        scene.lives--
-
-        if (!scene.lives) { 
-          setTimeout(gameOver(scene), 2000)
-        }
-      }
-    }
-  });
-  
-  itemSprite.on('pointerdown', () => {
-
-    if (itemSprite.isVirus) {
-      gameOver(scene)
-    } else if (scene.lives){
-      const score = scene.scoreText
-      const currentScore = score.text
-      scene.hitSound.play()
-
-      itemTween.stop()
-
-      const newItemTween = scene.tweens.add({
-        targets: itemSprite,
-        y: 350,
-        x: 200,
-        duration: 200,
-        ease: 'Sine',
-        onComplete: () => {
-          itemSprite.alpha = 0
-        }
-      });
-
-      score.setText(parseInt(currentScore) + 10)
-    }
-  });
-} 
-
-function getRandomNumberSignal() {
-  return (Math.random() - 0.5) * 2
-}
-
-function gameOver(scene) {
-  scene.backgroundSong.stop()
-  scene.gameOverSound.play()
-  alert('GAME OVER')
-  scene.scene.restart()
 }
